fb1cb27a4d97a74f71b8f0ae2b0ebf49c962a9eb
avdevelopers426 <81291527+avdevelopers426@users.noreply.github.com>
2023-06-01 17:00:24 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -3,17 +3,21 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import Select
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-#from flask import Flask, request
+from flask import Flask,request
 
 app = Flask(__name__)
 
-@app.route("/result",methods=["POST","GET"])
+@app.route("/result",methods=["POST"])
 
 def result():
-
-	#form_data = request.get_json()
+	data = request.json
+	print(data['a_length'])
+	
 	
 	chrome_options = Options()
 	chrome_options.add_argument('--headless')  # Run Chrome in headless mode (without opening the browser window)
@@ -27,49 +31,96 @@ def result():
 	# Navigate to the web page
 	driver.get(url)
 
-
 	# Find the form element using appropriate selectors (e.g., ID, class, etc.)
 	form = driver.find_element(By.ID, "planForm")
 
 	# Find the input fields within the form and fill in the desired values
 	input_field1 = form.find_element(By.NAME, "stocks[0].length")
-	input_field1.send_keys("1250")
+	input_field1.send_keys(data['a_length'])
 
 	input_field2 = form.find_element(By.NAME, "stocks[0].width")
-	input_field2.send_keys("2500")
+	input_field2.send_keys(data['a_width'])
 
 	input_field3 = form.find_element(By.NAME, "stocks[0].count")
-	input_field3.send_keys("2")
+	input_field3.send_keys(data['a_qty'])
 
+	input_field4 = form.find_element(By.NAME, "settings.kerf")
+	input_field4.send_keys(data['cut_width'])
+	
+	#elements = driver.find_elements(By.CLASS_NAME, ".card-body.py-1")
+	#elements2=elements[2].find_elements(By.CLASS_NAME, ".switcher")
+	grain = driver.find_element(By.ID, "settings.grain")
+	grain.find_element(By.XPATH, "..").click()
+	#elements2.click()
 
+	r_panel_values = data.get('r_panel', [])
+
+	x=0
+	js_code = """
+		addRequirementsRow2d(1, 'Delete', 'Horizontal', 'Vertical', true);
+		"""
+
+		# Call the JavaScript function
+	
+	#planReqTablebutton = driver.find_element(By.CSS_SELECTOR, ".d-xs-block .btn.mb-2:first-child")
+	#planReqTablebutton.click()
+	print(len(r_panel_values))
+	for panel in r_panel_values:
+		input_field1 = form.find_element(By.NAME, "requirements["+str(x)+"].length")
+		input_field1.send_keys(panel.get('r_length'))
+
+		input_field2 = form.find_element(By.NAME, "requirements["+str(x)+"].width")
+		input_field2.send_keys(panel.get('r_width'))
+
+		input_field3 = form.find_element(By.NAME, "requirements["+str(x)+"].count")
+		input_field3.send_keys(panel.get('r_qty'))
+
+		input_field4 = Select(form.find_element(By.NAME, "requirements["+str(x)+"].grainDirection"))
+		input_field4.select_by_value(panel.get('grainDirection'))
+		if len(r_panel_values)!=x+1:
+			driver.execute_script(js_code)
+		x=x+1;
+
+	#time.sleep(50)
+	try:
+		form.submit()
+	except StaleElementReferenceException:
+		return "error";
 	# Find the input fields within the form and fill in the desired values
-	input_field1 = form.find_element(By.NAME, "requirements[0].length")
-	input_field1.send_keys("297")
+	
+	
 
-	input_field2 = form.find_element(By.NAME, "requirements[0].width")
-	input_field2.send_keys("420")
-
-	input_field3 = form.find_element(By.NAME, "requirements[0].count")
-	input_field3.send_keys("40")
-
-
+	
 	#driver.implicitly_wait(500) 
 
 	#result = driver.page_source
 
 	# Submit the form
-	form.submit()
+	#form.submit()
 
 	# Wait for the page to load after form submission
 	#driver.implicitly_wait(10)  # Wait for 5 seconds (adjust as needed)
 	#driver.page_source
-	target_div = driver.find_element(By.CSS_SELECTOR, ".tab-pane.active > .card-body > table")
+	#time.sleep(50)
+	try:
+		target_div = driver.find_element(By.CSS_SELECTOR, ".tab-pane.active > .card-body > table")
+	except NoSuchElementException:
+		return "error";
+
 	div_html = target_div.get_attribute("outerHTML")
-	print(div_html)
+	#print(div_html)
+	if div_html:
+		return div_html;
+
+	"""if len(target_div) == 0:
+		return "error";
+
+	div_html = target_div.get_attribute("outerHTML")
+	#print(div_html)
 	if div_html:
 		return div_html;
 	else:
-		return "error";
+		return "error";"""
 
 
 #APP = result()
